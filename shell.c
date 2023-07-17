c40298c6ff33f90dcd2fb80c38a89068df4c2613
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2023-07-17 04:37:14 +0000

    up

--- shell.c
+++ shell.c
@@ -8,9 +8,10 @@ int main(int ac, char *av[])
 	pid_t pid;
 	int i, status;
 	ssize_t read;
-	char *buffer;
+	char *buffer, *shell;
 	size_t len;
 
+	shell = av[0];
 	status = TRUE;
 	while (status)
 	{
@@ -18,14 +19,22 @@ int main(int ac, char *av[])
 		len = 0;
 		write(STDOUT_FILENO, "($) ", 4);
 		read = getline(&buffer, &len, stdin);
-		if (read != -1)
+		if (read == -1)
 		{
+			if (feof(stdin))
+			{
+				write(STDIN_FILENO, "\n", 1);
+				exit(EXIT_SUCCESS);
+			}
+			dprintf(STDERR_FILENO, "%s: %s\n", shell, strerror(errno));
+			continue;
+		}
+		else
 			if (buffer[read - 1] == '\n')
 			{
 				buffer[read - 1] = '\0';
 				read--;
 			}
-		}
 		if (_strcmp((const char *)buffer, "exit"))
 		{
 			status = FALSE;
@@ -37,14 +46,14 @@ int main(int ac, char *av[])
 		pid = fork();
 		if (pid == -1)
 		{
-			dprintf(2,"can't fork process: %s\n", strerror(errno));
+			dprintf(STDERR_FILENO, "%s: %s\n", shell, strerror(errno));
 			continue;
 		}
 		if (pid == 0)
 		{
 			if ((execv(parsed->cmd, parsed->args)) == -1)
 			{
-				dprintf(2,"error: %s\n", strerror(errno));
+				dprintf(STDERR_FILENO, "%s: %s\n", shell, strerror(errno));
 				exit(EXIT_FAILURE);
 			}
 		}
