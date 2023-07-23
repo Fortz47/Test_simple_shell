654c27d8ee8af78034b6f872d69cc9cb11f23092
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-07-23 20:19:22 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -99,3 +99,23 @@ int return_num_of_arg(char *buffer)
 	free(buf);
 	return (argc);
 }
+
+/**
+ * check_valid - check if a file exists, is executable
+ * and is a regular file.
+ * @filepath: file to check
+ *
+ * Return: True(1), or False(0)
+ */
+int check_valid(char *filepath)
+{
+	struct stat sb;
+	int Access, st;
+
+	Access = access(filepath, X_OK);
+	st = stat(filepath, &sb);
+
+	if (!Access && !st && S_ISREG(sb.st_mode))
+		return (TRUE);
+	return (FALSE);
+}
