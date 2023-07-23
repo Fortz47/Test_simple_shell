061f4996be9dc10b2c5c5ad159694529be349569
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-07-23 20:16:50 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -56,3 +56,17 @@ int exec_cmd(parse *parsed, char **envp)
 	}
 	return (status);
 }
+
+/**
+ * free_arr_str - frees a 2D array
+ * @arr: 2d array
+ * @i: index stop
+ * @j: index start
+ */
+void free_arr_str(char **arr, int i, int j)
+{
+	int k;
+
+	for (k = j; k < i; k++)
+		free(arr[k]);
+}
