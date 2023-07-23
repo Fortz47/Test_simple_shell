009dbf94d8547667e06f1f64e01975941f30e69a
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-07-23 20:18:11 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -70,3 +70,32 @@ void free_arr_str(char **arr, int i, int j)
 	for (k = j; k < i; k++)
 		free(arr[k]);
 }
+
+/**
+ * return_num_of_arg - returns no. of args(strings) in
+ * an array
+ * @buffer: array
+ *
+ * Return: no of args(strings)
+ */
+int return_num_of_arg(char *buffer)
+{
+	char *buf, *token;
+	int argc;
+
+	argc = 0;
+	buf = _strdup(buffer);
+	if (!buf)
+		return (argc);
+	token = strtok(buf, " ");
+	if (token)
+		argc++;
+	while (token)
+	{
+		token = strtok(NULL, " ");
+		if (token)
+			argc++;
+	}
+	free(buf);
+	return (argc);
+}
